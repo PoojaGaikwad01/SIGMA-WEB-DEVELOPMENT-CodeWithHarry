7318bf010628777e46eb67dad037cb5ebca74185
PoojaGaikwad01 <poojagayakwad05@gmail.com>
2025-08-15 07:05:39 +0530

    JS TUTORIAL-56

---
+++ TUTORIAL-56/script.js
@@ -0,0 +1,33 @@
+console.log("Heyy! Welcome to TUTORIAL-56")
+let age = 0;
+grace = 2;
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+console.log(age % grace);
+if ((age + grace) > 18) {
+    console.log("You can drive")
+}
+else {
+    console.log("You cannot drive")
+}
+
+// ladder if
+if (age != 18) {
+    console.log("You can drive")
+}
+else if (age == 0) {
+    console.log("are you kidding")
+}
+else {
+    console.log("You cannot drive")
+}
+
+
+// 
+a = 6;
+b = 7;
+let c = (a > b) ? (a - b) : (b - a)
+console.log(c)
