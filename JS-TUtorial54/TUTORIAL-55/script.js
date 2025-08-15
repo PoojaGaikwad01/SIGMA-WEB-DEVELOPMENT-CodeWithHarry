ba05e8e770314eab0956855aacabac063d04b96c
PoojaGaikwad01 <poojagayakwad05@gmail.com>
2025-08-15 06:40:33 +0530

    JS tUTORIAL-55

---
+++ JS-TUtorial54/TUTORIAL-55/script.js
@@ -0,0 +1,30 @@
+console.log("Hey this is tutorial 55")
+
+var a = 5;
+var b = 7;
+var c = "Pooja";
+console.log(a + b + 8)
+console.log(typeof a, typeof b, typeof c)
+
+// Data types in js
+let z = "Pooja";
+let y = 3;
+let x = 3.22;
+// let w = true;
+let w = undefined;
+let t = null;
+const s = true;
+console.log(z, y, x, w, s)
+console.log(typeof z, typeof y, typeof x, typeof w, typeof t, typeof s)
+
+// Object in JS
+
+let o = {
+    "name": "Pooja",
+    "job role": "Developer"
+}
+console.log(o);
+o.salary = "100cr"
+console.log(o);
+o.salary = "500cr"
+console.log(o)
