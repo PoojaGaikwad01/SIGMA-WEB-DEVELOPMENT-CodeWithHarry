7dd8541101ae6a5ec7d039431e8f32f6e89e9b6f
PoojaGaikwad01 <poojagayakwad05@gmail.com>
2025-08-15 06:08:37 +0530

    JS TUTORIAL-54

---
+++ JS-TUtorial54/script.js
@@ -0,0 +1,17 @@
+alert("Hello World")
+
+console.log("Code is running...")
+console.log("Code is looking like a wow...")
+console.log("Code is also running...")
+
+var a = prompt("Enter your age")
+console.log("Your age is", +a)
+var isTrue = confirm("Aru you sure you want to leave this page and blast your computer")
+if (isTrue) {
+    console.log("Blasting your computer")
+}
+else {
+    console.log("Not Blasting your computer")
+}
+document.title = "Web by Pooja Gaikwad"
+document.body.style.backgroundColor = "darkblue"
