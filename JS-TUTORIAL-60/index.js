83456db0c2b1aebf5b1b3482abc6b899b15fed2b
PoojaGaikwad01 <poojagayakwad05@gmail.com>
2025-08-17 06:10:26 +0530

    JS TUTORIAL-60

---
+++ JS-TUTORIAL-60/index.js
@@ -0,0 +1,25 @@
+console.log("This is the string Tutorial")
+let a = "Pooja"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+let real_name = "POOJA"
+let friend = "RITU"
+console.log("My name is " + real_name + " and my friend's name is " + friend)
+console.log(`My name is ${real_name} and my friend's name is ${friend}`)
+
+let b = "Pooja Gaikwad"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 5))
+console.log(b.toLowerCase(1))
+
+console.log(b.replace("Pooja", "Payal"))
+
+console.log(b.concat(a, "NANI"))
