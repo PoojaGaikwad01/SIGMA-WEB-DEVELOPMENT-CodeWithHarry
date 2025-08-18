b9bb8433625a5c7a2adc1a432c20e2363ea6c4ea
PoojaGaikwad01 <poojagayakwad05@gmail.com>
2025-08-18 07:40:26 +0530

    JS-TUTORIAL-62-&-64

---
+++ JS-TUTORIAL-62-&-64/script.js
@@ -0,0 +1,12 @@
+let random = Math.random()
+let first, second, thired;
+if (rand < 0.33) {
+    first = "Crazy"
+
+}
+// elseif(rand < 0.66 && rand > 0.33){
+//     first = "Amazing"
+// }
+else{
+    first = "Fire"
+}
