1a02239826d93c15a96ac92e168a288e066f4982
Pooja Gaikwad <poojagayakwad05@gmail.com>
2025-08-19 19:21:05 +0530

    JS-TUTORIAL-71

---
+++ JS-TUTORIAL-71/script.js
@@ -0,0 +1,22 @@
+console.log(document.querySelector(".box").tagName)
+console.log(document.querySelector(".box").textContent)
+console.log(document.querySelector(".box").innerHTML = "Hey!, I am POOJA")
+console.log(document.querySelector(".conatainer").hidden)
+
+console.log(document.querySelector(".box").getAttribute("style"))
+console.log(document.querySelector(".box").setAttribute("style", "display:inline"))
+console.log(document.designMode = "on")
+console.log(document.querySelector(".box").dataset)
+// let div = document.createElement("div");
+// div.innerHTML = "I have Been inserted<b></b>"
+// div.setAttribute("class", "created");
+// document.querySelector(".conatainer").append(div)
+// console.log(document.querySelector(".box").remove())
+// console.log(document.querySelector(".box").remove())
+console.log(document.querySelector(".conatainer").classList)
+console.log(document.querySelector(".conatainer").className)
+console.log(document.querySelector(".conatainer").classList.add("pooja"))
+console.log(document.querySelector(".conatainer").classList)
+console.log(document.querySelector(".conatainer").className)
+// console.log(document.querySelector(".conatainer").remove("pooja"))
+console.log(document.querySelector(".conatainer").classList.toggle("red"))
