36c810bc8051d657072ec0efe06a41766f2345a0
Pooja Gaikwad <poojagayakwad05@gmail.com>
2025-08-19 20:09:20 +0530

    Exercise-13

---
+++ JS-TUTORIAL-73-&-77/script.js
@@ -0,0 +1,4 @@
+function createcard(title,cName, views, monthsOld, duration,thumbnail){
+
+}
+createcard("Introduction to Backend | Sigma Wen Dev video #2", "CodeWIthHarry", 560000, 7, "31:22","https://i.ytimg.com/vi/nXba2-mgn1k/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLAPrVzyLHFYcUIk5bv_LXaJrgOztA")
